90fb281721ac6669ade63f465a0f3c72a0be2a54
kimdongho <kimdongho@webtoonscorp.com>
2023-12-27 22:22:21 +0900

    remove any type parameter

--- src/routes/api/todo/complete/+server.ts
+++ src/routes/api/todo/complete/+server.ts
@@ -1,8 +1,8 @@
-import { get } from "svelte/store";
 import { tempTodo } from "../../../+page.server";
 import { Todo } from "../../../Todo";
+import type { RequestEvent } from "./$types";
 
-export const POST = async ({ request }: any) => {
+export const POST = async ({ request }: RequestEvent) => {
     const { completedAt } = await request.json();
     tempTodo.update((todo) => new Todo(todo.text, completedAt));
     return new Response();
--- src/routes/api/todo/update/+server.ts
+++ src/routes/api/todo/update/+server.ts
@@ -1,8 +1,8 @@
-import { get } from "svelte/store";
 import { tempTodo } from "../../../+page.server";
 import { Todo } from "../../../Todo";
+import type { RequestEvent } from "./$types";
 
-export const POST = async ({ request }: any) => {
+export const POST = async ({ request }: RequestEvent) => {
     const { todo } = await request.json();
     tempTodo.set(new Todo(todo));
     return new Response();
