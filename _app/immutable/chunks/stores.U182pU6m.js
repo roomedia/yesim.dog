66bad50df264bcd91bd1c93124eb08a7a5ad40c0
kimdongho <kimdongho@webtoonscorp.com>
2024-01-01 19:29:13 +0900

    Updates

--- _app/immutable/chunks/stores.U182pU6m.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./singletons.-P3MW-rl.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
